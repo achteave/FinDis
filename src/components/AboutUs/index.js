bd3b4e27c4665965bfe6bbc1a3dd56b00bd3ba60
Muhammad Affransyah Bayulaksana <124794209+achteave@users.noreply.github.com>
2023-05-26 14:14:40 +0700

    Create index.js

---
+++ src/components/AboutUs/index.js
@@ -0,0 +1,119 @@
+import './style.css';
+import franz from './assets/anz.png';
+import azizah from './assets/jijeh.png'
+import daffa from './assets/atap.jpg'
+import victor from './assets/tor.jpg'
+import { Icon, IconButton } from '@material-ui/core';
+import { ArrowBack } from '@material-ui/icons';
+import { useNavigate } from 'react-router-dom';
+
+const App = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <IconButton onClick={() => navigate(-1)}>
+        <ArrowBack />
+      </IconButton>
+      <div className="row">
+        <h1>Our Team</h1>
+      </div>
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <div className="img-container">
+              <img src={franz} alt="Team Member" />
+            </div>
+            <h3>M. Affransyah Bayulaksana</h3>
+            <p>Leader</p>
+            <div className="icons">
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="#">
+                <i className="fas fa-envelope"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <div className="img-container">
+              <img src={azizah} alt="Team Member" />
+            </div>
+            <h3>Nurul Azizah</h3>
+            <p>Member</p>
+            <div className="icons">
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="#">
+                <i className="fas fa-envelope"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <div className="img-container">
+              <img src={victor} alt="Team Member" />
+            </div>
+            <h3>Victorich Albert</h3>
+            <p>Member</p>
+            <div className="icons">
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="#">
+                <i className="fas fa-envelope"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <div className="img-container">
+              <img src={daffa} alt="Team Member" />
+            </div>
+            <h3>Daffa Althaf Maulana</h3>
+            <p>Member</p>
+            <div className="icons">
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="#">
+                <i className="fas fa-envelope"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
