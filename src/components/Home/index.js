1108fb91e4c5df592363727c47c9fc330b013e5f
Muhammad Affransyah Bayulaksana <124794209+achteave@users.noreply.github.com>
2023-05-27 12:55:54 +0700

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import logo from "./logo-0.png"
+import logo from "./logo-0.png";
 import "./style.css";
 
 const Home = () => {
