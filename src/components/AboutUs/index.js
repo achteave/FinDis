69d3567c3ebab8578a84dcf7fcbb7ab4bccf242a
Muhammad Affransyah Bayulaksana <124794209+achteave@users.noreply.github.com>
2023-05-28 16:04:08 +0700

    Update index.js

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,66 +1,80 @@
 import './style.css';
 import franz from './assets/anz.png';
-import azizah from './assets/jijeh.png'
-import daffa from './assets/atap.jpg'
+import azizah from './assets/jijeh.png';
+import daffa from './assets/atap.jpg';
 import victor from './assets/tor.jpg';
-import { useNavigate } from 'react-router-dom';
+import back from '../Assets/back.png';
+import { useNavigate, Link } from 'react-router-dom';
 
 const App = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <buutton className="" onClick={() => navigate(-1)}>
-        <img src='#' className='auback' alt='back'/>
-      </buutton>
-      <div className="row">
-        <h1>Our Team</h1>
-      </div>
-      <div className="row">
-        <div className="column">
-          <div className="card">
-            <div className="img-container">
-              <img src={franz} alt="Team Member" />
+    <div className='about'>
+      <div className='overlay'>
+          <img src={back} alt='back' className='auback' onClick={() => navigate(-1)}/>
+        <div className="row">
+          <h1>Our Team</h1>
+        </div>
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              <div className="img-container">
+                <img src={franz} alt="Team Member" />
+              </div>
+              <h3><b>M. Affransyah Bayulaksana</b></h3>
+              <p>Leader</p>
+              <p>00000077007</p>
+              <p>GitHub: @achteave</p>
+              <p></p>
             </div>
-            <h3>M. Affransyah Bayulaksana</h3>
-            <p>Leader</p>
-            <div className="icons">
+          </div>
+          <div className="column">
+            <div className="card">
+              <div className="img-container">
+                <img src={azizah} alt="Team Member" />
+              </div>
+              <h3><b>Nurul Azizah</b></h3>
+              <p>Member</p>
+              <p>00000076022</p>
+              <p>GitHub: @butzh</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="card">
+              <div className="img-container">
+                <img src={victor} alt="Team Member" />
+              </div>
+              <h3><b>Victorich Alber</b>t</h3>
+              <p>Member</p>
+              <p>00000077525</p>
+              <p>Github: @v_h_a_t</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="card">
+              <div className="img-container">
+                <img src={daffa} alt="Team Member" />
+              </div>
+              <h3><b>Daffa Althaf Maulana</b></h3>
+              <p>Member</p>
+              <p>00000076981</p>
+              <p>GitHub: @Altap115</p>
             </div>
           </div>
         </div>
-        <div className="column">
-          <div className="card">
-            <div className="img-container">
-              <img src={azizah} alt="Team Member" />
-            </div>
-            <h3>Nurul Azizah</h3>
-            <p>Member</p>
-            <div className="icons">
-            </div>
-          </div>
+        <footer class="footer-distributed">
+
+        <div class="footer-left">
+
+          <p class="footer-links">
+            <Link className="tohome" to={"/"}>Home</Link>
+            <Link className="toabout" to={"/aboutus"}>About Us</Link>
+          </p>
+          <p>UpToU Group &copy; 2023</p>
         </div>
-        <div className="column">
-          <div className="card">
-            <div className="img-container">
-              <img src={victor} alt="Team Member" />
-            </div>
-            <h3>Victorich Albert</h3>
-            <p>Member</p>
-            <div className="icons">
-            </div>
-          </div>
-        </div>
-        <div className="column">
-          <div className="card">
-            <div className="img-container">
-              <img src={daffa} alt="Team Member" />
-            </div>
-            <h3>Daffa Althaf Maulana</h3>
-            <p>Member</p>
-            <div className="icons">
-            </div>
-          </div>
-        </div>
+
+      </footer>
       </div>
     </div>
   );
