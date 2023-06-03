5385fbac69121af9bd9cf6a04c8e3feb88e40c5f
Muhammad Affransyah Bayulaksana <124794209+achteave@users.noreply.github.com>
2023-06-03 22:43:12 +0700

    Update index.js

--- src/components/Bookmarks/index.js
+++ src/components/Bookmarks/index.js
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import back from "../Assets/back.png";
 import "./style.css";
 
-const Bookmark = ({ bookmarks }) => {
+const Bookmark = ({ bookmarks, isBookmarked }) => {
   const navigate = useNavigate();
   
   return (
@@ -11,42 +11,47 @@ const Bookmark = ({ bookmarks }) => {
         <div className="pagebm">
           <div className="bookmark">
             <div className="list">
-                <img src={back} className="bmback" alt="back"onClick={() => navigate(-1)} />
+              <img src={back} className="bmback" alt="back" onClick={() => navigate(-1)} />
               <div className="title">
                 <h2 className="titlebm">Bookmarks</h2>
               </div>
             </div>
-              <div className="bmlist">
-                {Object.keys(bookmarks).length ? (
-                  Object.keys(bookmarks).map((b) => (
-                    <button
-                      className="listitem"
-                      key={b}
-                      onClick={() => navigate(`/search/${b}`)} // Updated onClick handler
-                    >
-                      <Link to={`/search/${b}`} className="bmword">
-                        {b}
-                      </Link>
-                    </button>
-                  ))
-                ) : (
-                  <p>No Bookmarks</p>
-                )}
-              </div>
+            <div className="bmlist">
+              {Object.keys(bookmarks).length ? (
+                Object.keys(bookmarks).map((b) => (
+                  <button
+                    className="listitem"
+                    key={b}
+                    onClick={() => navigate(`/search/${b}`)}
+                  >
+                    <Link to={`/search/${b}`} className="bmword">
+                      {b}
+                    </Link>
+                  </button>
+                ))
+              ) : (
+                <p>No Bookmarks</p>
+              )}
             </div>
+          </div>
         </div>
       </div>
       <footer class="footer-distributed">
-
-			<div class="footer-left">
-
-				<p class="footer-links">
-          <Link className="tohome" to={"/"}>Home</Link>
-					<Link className="toabout" to={"/aboutus"}>About Us</Link>
-				</p>
-				<p>UpToU Group &copy; 2023</p>
-			</div>
-		</footer>
+        <div class="footer-left">
+          <p class="footer-links">
+            <Link className="tohome" to={"/"}>
+              Home
+            </Link>
+            <Link className="toabout" to={"/aboutus"}>
+              About Us
+            </Link>
+          </p>
+          <p>UpToU Group &copy; 2023</p>
+        </div>
+      </footer>
+      {isBookmarked && (
+        <p style={{ textAlign: "center" }}>No synonyms/antonyms available.</p>
+      )}
     </div>
   );
 };
